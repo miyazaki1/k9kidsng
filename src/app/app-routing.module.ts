2c41662e1afc66630f4cfd0cf792e061cbcc3a3b
keisuke_miyazaki <mint@momoirosoft.moe>
2019-05-09 11:24:57 -0400

    An Angry Clownfish fixed the comonent and service hierarchy

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,10 @@
 import { NgModule } from "@angular/core";
 import { Routes, RouterModule } from "@angular/router";
-import { RegisterComponent } from "./register/register/register.component";
-import { LoginComponent } from "./login/login/login.component";
-import { AddDogComponent } from "./add-dog/add-dog/add-dog.component";
-import { EditDogComponent } from "./edit-dog/edit-dog/edit-dog.component";
-import { ListDogComponent } from "./list-dog/list-dog.component";
+import { RegisterComponent } from "./components/register/register/register.component";
+import { LoginComponent } from "./components/login/login/login.component";
+import { AddDogComponent } from "./components/add-dog/add-dog/add-dog.component";
+import { EditDogComponent } from "./components/edit-dog/edit-dog/edit-dog.component";
+import { ListDogComponent } from "./components/list-dog/list-dog.component";
 
 const routes: Routes = [
   { path: "", redirectTo: "/login", pathMatch: "full" },
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,15 +4,15 @@ import { NgModule } from "@angular/core";
 import { AppRoutingModule } from "./app-routing.module";
 import { AppComponent } from "./app.component";
 import { FormsModule, ReactiveFormsModule } from "@angular/forms";
-import { RegisterComponent } from "./register/register/register.component";
-import { LoginComponent } from "./login/login/login.component";
-import { AddDogComponent } from "./add-dog/add-dog/add-dog.component";
-import { EditDogComponent } from "./edit-dog/edit-dog/edit-dog.component";
-import { ListDogComponent } from "./list-dog/list-dog.component";
-import { DogService } from "./service/dog.service";
-import { AuthenticationService } from "./service/auth.service";
+import { RegisterComponent } from "./components/register/register/register.component";
+import { LoginComponent } from "./components/login/login/login.component";
+import { AddDogComponent } from "./components/add-dog/add-dog/add-dog.component";
+import { EditDogComponent } from "./components/edit-dog/edit-dog/edit-dog.component";
+import { ListDogComponent } from "./components/list-dog/list-dog.component";
+import { DogService } from "./services/dog.service";
+import { AuthenticationService } from "./services/auth.service";
 import { HttpClientModule, HttpClient } from "@angular/common/http";
-import { PetfinderService } from "./service/petfinder.service";
+import { PetfinderService } from "./services/petfinder.service";
 import { LoaderComponent } from './shared/loader/loader.component';
 
 @NgModule({
