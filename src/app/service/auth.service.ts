50511e80cd6e50c9beef054979e78551e4177303
Xiomaira Inga <xiomairajinga@gmail.com>
2019-05-12 16:26:34 -0400

    login completed and tested

--- src/app/service/auth.service.ts
+++ src/app/service/auth.service.ts
@@ -7,22 +7,22 @@ import { map } from "rxjs/operators";
 export class AuthenticationService {
   constructor(private http: HttpClient) {}
 
-  login(email: string, password: string) {
+  login(username: string, password: string): Observable <any> {
     return this.http
-      .post<any>("/api/authenticate", {
-        email: email,
+      .post<any>("http://localhost:8088/brooks/account/login", {
+        username: username,
         password: password
       })
-      .pipe(
-        map(user => {
-          // login successful if there's a jwt token in the response
-          if (user && user.token) {
-            // store user details and jwt token in local storage to keep user logged in between page refreshes
-            localStorage.setItem("currentUser", JSON.stringify(user));
-          }
+      // .pipe(
+      //   map(user => {
+      //     // login successful if there's a jwt token in the response
+      //     if (user && user.token) {
+      //       // store user details and jwt token in local storage to keep user logged in between page refreshes
+      //       localStorage.setItem("currentUser", JSON.stringify(user));
+      //     }
 
-          return user;
-        })
-      );
+      //     return user;
+      //   })
+      // );
   }
 }
